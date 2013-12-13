84dec95f62a5b5f4815e8e6612a519a9e44fd9eb
firepick1 <karl@firepick.org>
2013-12-12 16:54:19 -0800

    windows-service-fix

--- win-service.bat
+++ win-service.bat
@@ -1,8 +1,23 @@
 @echo off
+if "%1"=="uninstall" goto UNINSTALL
+if "%1"=="install" goto INSTALL
+echo Example:
+echo   win-service install
+echo   win-service uninstall
+goto END
+
+:UNINSTALL
 echo uninstalling Windows service
-net stop seedapp.exe
-sc delete seedapp.exe
+if exist daemon net stop SEED_APP
+if exist daemon sc delete seed_app.exe
 if exist daemon del /q daemon\*.*
+rmdir daemon
+goto END
+
+:INSTALL
 set SEED_APP=%~dp0
 echo installing Windows service with SEED_APP=%SEED_APP%
 node win-service.js
+goto END
+
+:END
